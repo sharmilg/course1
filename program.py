64bf42226dcf6e76dbf709c6c4eefd556b7a5de3
sharmilg <sharmilg@metropolia.fi>
2024-08-22 10:40:35 +0300

    Initail Comit, Variables are defind

---
+++ program.py
@@ -0,0 +1,11 @@
+intvariable= 4
+floatvariable= 4.5
+stringvariable= "Software 1 is fun"
+
+print(intvariable)
+print(floatvariable)
+print(stringvariable)
+
+print(type(intvariable))
+print(type(floatvariable))
+print(type(stringvariable))
